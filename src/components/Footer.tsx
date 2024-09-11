675195629de1342b7857adff9937c3323c9cf753
Your Name <your.email@example.com>
2024-09-11 13:24:36 +0200

    Added tip jar and API info to the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,4 @@
+// src/components/Footer.tsx
 import React from 'react';
 import Link from 'next/link';
 
@@ -5,12 +6,14 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-800 text-white p-4">
       <div className="container mx-auto text-center">
-        <p>
+        <p className="mb-2">
           © Copyright 2024, created by {' '}
           <Link href="https://x.com/0xPhantom_" target="_blank" rel="noopener noreferrer">
             Phantom
           </Link>
         </p>
+        <p className="mb-2">BTC data Provided by minerstat®</p>
+        <p>BTC TIP Jar: bc1qv9udhuqvh9wpgf79tc5v2e98sux03chghanzn4</p>
       </div>
     </footer>
   );
